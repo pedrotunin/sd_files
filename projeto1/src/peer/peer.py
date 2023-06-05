d164b64276cfe7fb5eb045c0df5047733b57d962
Pedro Tunin <pedrotunin@gmail.com>
2023-06-05 10:58:14 -0300

    Adicionando estrutura inicial

---
+++ projeto1/src/peer/peer.py
@@ -0,0 +1,9 @@
+import socket
+
+class Peer:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+if __name__ == "__main__":
+    pass
---
+++ projeto1/src/server/server.py
@@ -0,0 +1,9 @@
+import socket
+
+class Server:
+    def __init__(self, host, port):
+        self.host = host
+        self.port = port
+
+if __name__ == "__main__":
+    pass
