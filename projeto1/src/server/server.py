7570cd754482eac1e7c2093219eadc865fa1a51a
Pedro Tunin <pedrotunin@gmail.com>
2023-06-06 20:15:18 -0300

    Adicionando função run e corpo da main

--- projeto1/src/server/server.py
+++ projeto1/src/server/server.py
@@ -4,6 +4,24 @@ class Server:
     def __init__(self, host, port):
         self.host = host
         self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    def run(self):
+        print("Servidor rodando em {}:{}".format(self.host, self.port))
+
+        self.socket.bind((self.host, self.port))
+        self.socket.listen(5)
+
+        while True:
+            client_socket, address = self.socket.accept()
+            pass
 
 if __name__ == "__main__":
-    pass
+
+    print("Insira o endereco IP do servidor: ")
+    host = input()
+    print("Insira a porta do servidor: ")
+    port = int(input())
+
+    server = Server(host, port)
+    server.run()
