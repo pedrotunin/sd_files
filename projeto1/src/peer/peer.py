a6fbaaeb29ff9a864930c4f5fa3fec8098f14467
Pedro Tunin <pedrotunin@gmail.com>
2023-06-25 19:36:04 -0300

    Adicionando função de download entre peers

--- projeto1/src/peer/peer.py
+++ projeto1/src/peer/peer.py
@@ -1,9 +1,10 @@
 import os
+import math
 import json
 import socket
 import threading
 
-from concurrent.futures import ThreadPoolExecutor
+PART_SIZE = 1024 * 16
 
 class Peer:
     def __init__(self, host, port, folder):
@@ -11,35 +12,69 @@ class Peer:
         self.port = port
         self.folder = folder
         self.files = self.list_files()
-        self.socket = None
-        self.create_socket()
         
-        self.socket_listen = None
+        self.socket = None
+        
         self.socket_listen = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket_listen.bind((self.host, 23456))
+        self.socket_listen.bind((self.host, self.port))
         
         self.thread = threading.Thread(target=self.run, args=(1,), daemon=True)
         self.thread.start()
-        
-    def run(self, name):
-        try:
-            self.socket_listen.listen(5)
-            
-            while True:
-                client_socket, address = self.socket_listen.accept()
-                host, port = address
-                
-                req = client_socket.recv(1024).decode()
-                
-                res = "Recebida mensagem do peer {}:{}".format(host, port)
-                
-                print(res)
-                
-                client_socket.close()
-            
-        finally:
-            self.socket_listen.close()
 
+    def run(self, name):
+        self.socket_listen.listen(5)
+            
+        for i in range(5):
+            t = threading.Thread(target=self.handle_connection, args=(i,), daemon=True)
+            t.start()
+
+    def handle_connection(self, name):
+        while True:
+            client_socket, address = self.socket_listen.accept()
+                    
+            req = client_socket.recv(1024).decode()
+            req = json.loads(req)
+
+            if req["action"] == "DOWNLOAD":
+                self.handle_upload(req["data"], client_socket)
+                    
+            client_socket.close()
+
+    def handle_upload(self, data: dict, client_socket: socket.socket) -> None:
+        if not self.check_file_exists(data["file"]):
+            res = json.dumps({
+                "message": "FILE_NOT_FOUND",
+                "data": {
+                    "host": self.host,
+                    "port": self.port,
+                    "number_of_parts": 0
+                }            
+            })
+            client_socket.send(res.encode())
+            
+        else: # o arquivo existe
+            file_path = os.path.join(self.folder, data["file"])
+            file_size = os.path.getsize(file_path)
+            number_of_parts = self.get_number_of_parts(file_size)
+
+            res = json.dumps({
+                "message": "FILE_FOUND",
+                "data": {
+                    "host": self.host,
+                    "port": self.port,
+                    "file_size": file_size,
+                    "number_of_parts": number_of_parts
+                }
+            })
+
+            client_socket.send(res.encode())
+
+            req = client_socket.recv(1024).decode() # espera o OK do cliente
+            req = json.loads(req)
+
+            if req["message"] == "DOWNLOAD_READY":
+                self.upload(file_path, number_of_parts, client_socket)
+        
     def join(self, server_host, server_port):
         self.create_socket()
         self.socket.connect((server_host, server_port))
@@ -65,9 +100,23 @@ class Peer:
 
         self.socket.close()
 
-    def update(self):
-        #TODO: implementar update no servidor
-        pass
+    def update(self, file: str, server_host, server_port):
+        self.create_socket()
+        self.socket.connect((server_host, server_port))
+
+        req = json.dumps({
+            "action": "UPDATE",
+            "data": {
+                "host": self.host,
+                "port": self.port,
+                "file": file
+            }
+        })
+
+        self.socket.send(req.encode())
+
+        res = self.socket.recv(1024).decode()
+        res = json.loads(res)
 
     def search(self, file: str, server_host, server_port):
         self.create_socket()
@@ -76,6 +125,8 @@ class Peer:
         req = json.dumps({
             "action": "SEARCH",
             "data": {
+                "host": self.host,
+                "port": self.port,
                 "file": file
             }
         })
@@ -91,13 +142,60 @@ class Peer:
 
         self.socket.close()
 
-    def download(self, host: str, port: int, file: str):
-        #TODO: implementar requisição de download de arquivos para outros peers
-        pass
+    def download(self, host: str, port: int, file_name: str):
+        self.create_socket()
+        self.socket.connect((host, port))
 
-    def upload(self, host: str, port: str, file: str):
-        #TODO: implementar upload de arquivos para outros peers
-        pass
+        req = json.dumps({
+            "action": "DOWNLOAD",
+            "data": {
+                "host": self.host,
+                "port": self.port,
+                "file": file_name
+            }
+        })
+
+        self.socket.send(req.encode())
+
+        res = self.socket.recv(1024).decode()
+        res = json.loads(res)
+
+        if res["message"] == "FILE_NOT_FOUND":
+            print("\nArquivo não encontrado")
+            return
+        
+        req = json.dumps({
+            "message": "DOWNLOAD_READY",
+            "data": {
+                "host": self.host,
+                "port": self.port
+            }
+        })
+
+        self.socket.send(req.encode())
+
+        file_path = os.path.join(self.folder, file_name)
+
+        with open(file_path, "wb") as file:
+            for i in range(res["data"]["number_of_parts"]):
+                part = self.socket.recv(PART_SIZE)
+                file.write(part)
+
+                self.socket.send("Dados recebidos".encode())
+
+            print("\nArquivo {} baixado com sucesso na pasta {}\n".format(file_name, self.folder))
+            self.update(file_name, "127.0.0.1", 1099)
+
+    def upload(self, file_path: str, number_of_parts: int, client_socket: socket.socket) -> None:
+        
+        with open(file_path, "rb") as file:
+            for i in range(number_of_parts):
+                part = file.read(PART_SIZE)
+                client_socket.send(part)
+                msg = client_socket.recv(1024).decode()
+
+    def get_number_of_parts(self, file_size: int) -> int:
+        return math.ceil(file_size / PART_SIZE)
 
     def check_file_exists(self, file: str) -> bool:
         files = self.list_files()
@@ -134,7 +232,7 @@ class Peer:
     
     def create_socket(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.bind((self.host, self.port))
+        #self.socket.bind((self.host, self.port))
 
 if __name__ == "__main__":
 
@@ -175,10 +273,10 @@ if __name__ == "__main__":
             peer.search(file, "127.0.0.1", 1099)
 
         elif option == "3": # DOWNLOAD
-            print("Insira o IP do peer que possui o arquivo: ")
-            ip_peer = "127.0.0.1" #input()
+            print("Insira o IP do peer que possui o arquivo: ", end="")
+            ip_peer = input()
 
-            print("Insira a porta do peer que possui o arquivo: ")
-            port_peer = 12346 #int(input())
+            print("Insira a porta do peer que possui o arquivo: ", end="")
+            port_peer = int(input())
 
             peer.download(ip_peer, port_peer, file)
